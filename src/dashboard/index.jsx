1098a1bf80608f45f46867c23b1f84eacbe17331
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-22 11:32:33 +0300

    created the addcv button on the dashboard

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import CreateCv from './components/CreateCv'
 function Dashboard() {
   return (
     <div className='p-10 md:px-20 lg:px-32'>
       <h2 className='font-bold text-3xl'>My Resume</h2>
       <p>Create an amazing CV for your next job opportunity</p>
+    <div>
+      <CreateCv />
+    </div>
     </div>
   )
 }
