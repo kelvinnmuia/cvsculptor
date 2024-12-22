31a2fb5f8a359e26ffa08a714b6c6d3c31722d2f
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-22 11:49:52 +0300

    added padding to the AddCv button

--- src/dashboard/components/AddCv.jsx
+++ src/dashboard/components/AddCv.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function AddCv() {
   return (
     <div>
-        <div>
+        <div className='p-14 py-24 border 
+        items-center flex 
+        justify-center bg-secondary
+        rounded-lg'>
             <PlusSquare />
         </div>
     </div>
