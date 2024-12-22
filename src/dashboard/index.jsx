662507ef9243b800e4c55545dd174f67cf3f90ea
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-22 11:13:05 +0300

    Increased the font weight and size for the dashboard header

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='p-10 md:px-20 lg:px-32'>
-      <h2>My Resume</h2>
+      <h2 className='font-bold text-2xl'>My Resume</h2>
     </div>
   )
 }
