8481f4f3bd4dcc30f2a57f680438b825231bac86
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-22 11:56:20 +0300

    changed the dashboard header to My CV

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,7 +3,7 @@ import AddCv from './components/AddCv'
 function Dashboard() {
   return (
     <div className='p-10 md:px-20 lg:px-32'>
-      <h2 className='font-bold text-3xl'>My Curriculum Vitae</h2>
+      <h2 className='font-bold text-3xl'>My CV</h2>
       <p>Create an amazing CV for your next job opportunity</p>
     <div className="grid grid-cols-2 
     md:grid-cols-3 lg:grid-cols-5
