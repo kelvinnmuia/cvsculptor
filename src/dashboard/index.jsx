1c469b18e38a9b973cb1a462ae0423d6d35781d9
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-22 11:18:42 +0300

    added a paragraph beneath the dashboard header

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='p-10 md:px-20 lg:px-32'>
-      <h2 className='font-bold text-2xl'>My Resume</h2>
+      <h2 className='font-bold text-3xl'>My Resume</h2>
+      <p>Create an amazing CV for your next job opportunity</p>
     </div>
   )
 }
