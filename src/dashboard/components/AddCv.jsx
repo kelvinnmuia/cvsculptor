3eef60be18a8fea9d898430bde7300e082089f2c
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-22 11:43:10 +0300

    added the plus squre to the addcv button

---
+++ src/dashboard/components/AddCv.jsx
@@ -0,0 +1,13 @@
+import { PlusSquare } from 'lucide-react'
+import React from 'react'
+function AddCv() {
+  return (
+    <div>
+        <div>
+            <PlusSquare />
+        </div>
+    </div>
+  )
+}
+
+export default AddCv
--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import CreateCv from './components/CreateCv'
+import AddCv from './components/AddCv'
 function Dashboard() {
   return (
     <div className='p-10 md:px-20 lg:px-32'>
       <h2 className='font-bold text-3xl'>My Resume</h2>
       <p>Create an amazing CV for your next job opportunity</p>
-    <div>
-      <CreateCv />
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+      <AddCv />
     </div>
     </div>
   )
