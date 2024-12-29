c4fecf337414c7b21a83e7f5d00489b8b3776a2b
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-29 17:52:20 +0300

    changed the cv card border to rounded

--- src/dashboard/components/CvCardItem.jsx
+++ src/dashboard/components/CvCardItem.jsx
@@ -14,7 +14,8 @@ function CvCardItem({cv}) {
   return (
     <div>
       <div className='p-14 bg-secondary flex
-      items-center justify-center h-[280px] border border-primary'>
+      items-center justify-center h-[280px] 
+      border border-primary rounded-lg'>
         <Notebook />
       </div>
       <h2 className='text-center my-1'>{cv.title}</h2>
