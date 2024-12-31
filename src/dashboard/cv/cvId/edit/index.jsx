ce482ce646a2ded99a2b4a505059fdad55e7cc18
kelvinnmuia <kelvinnmuia@gmail.com>
2024-12-31 19:53:35 +0300

    added the form preview section

--- src/dashboard/cv/cvId/edit/index.jsx
+++ src/dashboard/cv/cvId/edit/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import FormSection from './components/FormSection';
 
 function EditCv() {
     const params=useParams();
@@ -8,8 +9,11 @@ function EditCv() {
         console.log(params.cvId);
     },[])
   return (
-    <div>
-      EditCv
+    <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
+      {/* Form Section */}
+      <FormSection/>
+      {/* Preview Section */}
+
     </div>
   )
 }
