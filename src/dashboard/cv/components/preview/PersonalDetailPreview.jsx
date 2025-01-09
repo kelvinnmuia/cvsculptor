b7f30d78060be85f4ddfd40e182213f9a6e1e157
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-09 11:21:17 +0300

    added the theme color to the job title header

--- src/dashboard/cv/components/preview/PersonalDetailPreview.jsx
+++ src/dashboard/cv/components/preview/PersonalDetailPreview.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 function PersonalDetailPreview({cvInfo}) {
   return (
     <div>
-      <h2 className='font-bold text-xl text-center'>
+      <h2 className='font-bold text-xl text-center'
+      style={{
+        color:cvInfo?.themeColor
+      }}
+      >
         {cvInfo?.firstName} {cvInfo?.lastName}</h2>
-      <h2 className='text-center text-sm font-medium'>{cvInfo?.jobTitle}</h2>
+      <h2 className='text-center text-sm font-medium'
+      style={{
+        color:cvInfo?.themeColor
+      }}
+      >{cvInfo?.jobTitle}</h2>
     </div>
   )
 }
