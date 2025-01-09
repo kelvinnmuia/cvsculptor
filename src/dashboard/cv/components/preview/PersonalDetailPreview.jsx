d36601f5d20f4b0a6368e837ff9b65bd549c6db2
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-09 11:06:40 +0300

    added the job title header on the personal details preview section

--- src/dashboard/cv/components/preview/PersonalDetailPreview.jsx
+++ src/dashboard/cv/components/preview/PersonalDetailPreview.jsx
@@ -5,6 +5,7 @@ function PersonalDetailPreview({cvInfo}) {
     <div>
       <h2 className='font-bold text-xl text-center'>
         {cvInfo?.firstName} {cvInfo?.lastName}</h2>
+      <h2 className='text-center text-sm font-medium'>{cvInfo?.jobTitle}</h2>
     </div>
   )
 }
