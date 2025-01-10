3b898c40116eea33dc7c798d1c892dffefcfc3da
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-10 11:59:27 +0300

    added the personal detail form into the forms section

--- src/dashboard/cv/components/FormSection.jsx
+++ src/dashboard/cv/components/FormSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import PersonalDetail from './forms/PersonalDetail'
 
 function FormSection() {
   return (
     <div>
-      FormSection
+      {/* Personal Detail */}
+      <PersonalDetail />
+      {/* Summary */}
+
+      {/* Experience */}
+
+      {/* Educational Detail */}
+
+      {/* Skills */}
+
     </div>
   )
 }
