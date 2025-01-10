c4f0f843896cbd3804a685ebba6e6808e7f31e21
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-10 12:05:53 +0300

    added a header to the personal detail form

--- src/dashboard/cv/components/forms/PersonalDetail.jsx
+++ src/dashboard/cv/components/forms/PersonalDetail.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { CvInfoContext } from '@/context/CvInfoContext'
+import React, { useContext } from 'react'
 
 function PersonalDetail() {
+    const [cvInfo, setCvInfo]=useContext(CvInfoContext)
   return (
-    <div>
-      
+    <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
+        <h2 className='font-bold text-lg'>Personal Detail</h2>
+        <p>Get Started with the basic information</p>
     </div>
   )
 }
