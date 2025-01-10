ff839f6279c18cb195ca37644f282bacc882de70
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-10 13:14:29 +0300

    added a firstname input field to the personal details form

--- src/dashboard/cv/components/forms/PersonalDetail.jsx
+++ src/dashboard/cv/components/forms/PersonalDetail.jsx
@@ -1,3 +1,4 @@
+import { Input } from '@/components/ui/input'
 import { CvInfoContext } from '@/context/CvInfoContext'
 import React, { useContext } from 'react'
 
@@ -7,6 +8,15 @@ function PersonalDetail() {
     <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
         <h2 className='font-bold text-lg'>Personal Detail</h2>
         <p>Get Started with the basic information</p>
+
+        <form>
+            <div className='grid grid-cols-2 mt-5 gap-3'>
+                <div>
+                    <label className='text-sm'>First Name</label>
+                    <Input name="firstName"/>
+                </div>
+            </div>
+        </form>
     </div>
   )
 }
