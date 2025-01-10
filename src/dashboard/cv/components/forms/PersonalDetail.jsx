0804dc567f3a87b20f596e7b42a6669fbfa74f02
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-10 13:41:03 +0300

    added the address field to the personal details form

--- src/dashboard/cv/components/forms/PersonalDetail.jsx
+++ src/dashboard/cv/components/forms/PersonalDetail.jsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { CvInfoContext } from '@/context/CvInfoContext'
 import React, { useContext } from 'react'
 
 function PersonalDetail() {
     const {cvInfo,setCvInfo}=useContext(CvInfoContext)
+
+    const handleInputChange=(e)=>{
+
+    }
   return (
     <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
         <h2 className='font-bold text-lg'>Personal Detail</h2>
@@ -13,8 +18,31 @@ function PersonalDetail() {
             <div className='grid grid-cols-2 mt-5 gap-3'>
                 <div>
                     <label className='text-sm'>First Name</label>
-                    <Input name="firstName"/>
+                    <Input name="firstName" required onChange={handleInputChange}/>
                 </div>
+                <div>
+                    <label className='text-sm'>Last Name</label>
+                    <Input name="lastName" required onChange={handleInputChange}/>
+                </div>
+                <div className='col-span-2'>
+                    <label className='text-sm'>Job Title</label>
+                    <Input name="jobTitle" required onChange={handleInputChange}/>
+                </div>
+                <div className='col-span-2'>
+                    <label className='text-sm'>Address</label>
+                    <Input name="address" required onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label className='text-sm'>Phone</label>
+                    <Input name="phone" required onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label className='text-sm'>Email</label>
+                    <Input name="email" required onChange={handleInputChange}/>
+                </div>
+            </div>
+            <div>
+                <Button>Save</Button>
             </div>
         </form>
     </div>
