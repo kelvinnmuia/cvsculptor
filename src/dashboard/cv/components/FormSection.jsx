5a64aee5c5f1104ae050796031699c1c37492c13
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-10 12:32:12 +0300

    added theme and next button on the form section

--- src/dashboard/cv/components/FormSection.jsx
+++ src/dashboard/cv/components/FormSection.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import PersonalDetail from './forms/PersonalDetail'
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
 
 function FormSection() {
   return (
     <div>
+      <div>
+        <Button>Theme</Button>
+        <div>
+          <Button className="flex gap-2" size="sm"> Next <ArrowRight/> </Button>
+        </div>
+      </div>
       {/* Personal Detail */}
-      <PersonalDetail />
+      <PersonalDetail/>
       {/* Summary */}
 
       {/* Experience */}
--- src/dashboard/cv/components/forms/PersonalDetail.jsx
+++ src/dashboard/cv/components/forms/PersonalDetail.jsx
@@ -2,7 +2,7 @@ import { CvInfoContext } from '@/context/CvInfoContext'
 import React, { useContext } from 'react'
 
 function PersonalDetail() {
-    const [cvInfo, setCvInfo]=useContext(CvInfoContext)
+    const {cvInfo,setCvInfo}=useContext(CvInfoContext)
   return (
     <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
         <h2 className='font-bold text-lg'>Personal Detail</h2>
