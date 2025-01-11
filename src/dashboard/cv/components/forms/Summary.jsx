db152f23951ba436592be60b21983aa8121d8915
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-11 18:46:29 +0300

    added gemmini api key to generate cv professional summary

--- src/dashboard/cv/components/forms/Summary.jsx
+++ src/dashboard/cv/components/forms/Summary.jsx
@@ -6,11 +6,15 @@ import { useParams } from 'react-router-dom';
 import GlobalApi from './../../../../../service/GlobalApi';
 import { LoaderCircle, Brain } from 'lucide-react';
 import { toast } from 'sonner';
+import { AIChatSession } from './../../../../../service/AIModel';
+
+const prompt="Job Title: {jobTitle}, Depends on job title give me summary for my cv within 4 - 5 lines in JSON format with field experience Level and Summary with Experience level for Fresher, Mid-Level, Experienced."
 function Summary({enabledNext}) {
     const {cvInfo,setCvInfo}=useContext(CvInfoContext);
     const [summary,setSummary]=useState();
     const [loading,setLoading]=useState(false);
     const params=useParams();
+    const [aiGeneratedSummaryList,setAiGeneratedSummaryList]=useState();
 
     useEffect(()=>{
         summary&&setCvInfo({
@@ -18,6 +22,16 @@ function Summary({enabledNext}) {
             summary:summary
         })
     },[summary])
+
+    const GenerateSummaryFromAI=async()=>{
+        setLoading(true);
+        const PROMPT=prompt.replace("{jobTitle}",cvInfo?.jobTitle);
+        console.log(PROMPT);
+        const result=await AIChatSession.sendMessage(PROMPT);
+        console.log(JSON.parse(result.response.text()));
+        setAiGeneratedSummaryList(JSON.parse(result.response.text()));
+        setLoading(false);
+    }
 
     const onSave=(e)=>{
         e.preventDefault();
@@ -48,7 +62,7 @@ function Summary({enabledNext}) {
         <form className="mt-7" onSubmit={onSave}>
             <div className="flex justify-between items-end">
                 <label>Add Summary</label>
-                <Button variant="outline" type="button"size="sm" className="border-primary text-primary flex gap-2"> 
+                <Button variant="outline" onClick={()=>GenerateSummaryFromAI()} type="button"size="sm" className="border-primary text-primary flex gap-2"> 
                     <Brain className='h-4 w-4'/> Generate from AI</Button>
             </div>
             <Textarea className="mt-5" required
