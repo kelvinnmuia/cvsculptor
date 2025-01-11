a1d58dfa19bb812340fb3d70424485e0699bd892
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-11 16:09:06 +0300

    changed the content of the summary section heading and subheading

--- src/dashboard/cv/components/forms/Summary.jsx
+++ src/dashboard/cv/components/forms/Summary.jsx
@@ -4,8 +4,8 @@ function Summary() {
   return (
     <div>
       <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
-        <h2 className='font-bold text-lg'>Personal Detail</h2>
-        <p>Get Started with the basic information</p>
+        <h2 className='font-bold text-lg'>Summary</h2>
+        <p>Add Summary for your job title</p>
       </div>
     </div>
   )
