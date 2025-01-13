7b47dd268b89469d3b917bd298265f8b13952bb0
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-13 13:34:12 +0300

    added the map function to iterate through the professional experience fields

--- src/dashboard/cv/components/forms/Experience.jsx
+++ src/dashboard/cv/components/forms/Experience.jsx
@@ -20,6 +20,15 @@ function Experience() {
         <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
         <h2 className='font-bold text-lg'>Professional Experience</h2>
         <p>Add Your Previous Job(s) Experience</p>
+        <div>
+            {experienceList.map((item,index)=>(
+                <div>
+                    <div className='grid grid-cols-2'>
+
+                    </div>
+                </div>
+            ))}
+        </div>
         </div>
     </div>
   )
