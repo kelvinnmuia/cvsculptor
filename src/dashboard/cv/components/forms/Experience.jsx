ec1135e646636e053df8e24ad930248970cc2a98
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-13 13:24:33 +0300

    changed the content of the heading and subheading in the professional experience section

--- src/dashboard/cv/components/forms/Experience.jsx
+++ src/dashboard/cv/components/forms/Experience.jsx
@@ -18,8 +18,8 @@ function Experience() {
   return (
     <div>
         <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
-        <h2 className='font-bold text-lg'>Summary</h2>
-        <p>Add Summary for your job title</p>
+        <h2 className='font-bold text-lg'>Professional Experience</h2>
+        <p>Add Your Previous Job(s) Experience</p>
         </div>
     </div>
   )
