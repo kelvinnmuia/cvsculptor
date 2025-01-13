cf412676a9d6ada4b07d4f40a9d71c0a59d13671
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-13 13:19:52 +0300

    added the heading and subheading for the experience section

--- src/dashboard/cv/components/forms/Experience.jsx
+++ src/dashboard/cv/components/forms/Experience.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+const formField={
+    title:'',
+    companyName:'',
+    city:'',
+    state:'',
+    startDate:'',
+    endDate:'',
+    workSummary:''
+}
 function Experience() {
+    const [experienceList,setExperienceList]=useState([
+        {
+            formField
+        }
+    ])
   return (
     <div>
-      
+        <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
+        <h2 className='font-bold text-lg'>Summary</h2>
+        <p>Add Summary for your job title</p>
+        </div>
     </div>
   )
 }
