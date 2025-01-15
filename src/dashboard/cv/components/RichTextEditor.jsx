7e4b158b32ff2a34cde69ad1fe7072737869fd7e
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-15 15:19:33 +0300

    added generate from ai button on the rich text editor

--- src/dashboard/cv/components/RichTextEditor.jsx
+++ src/dashboard/cv/components/RichTextEditor.jsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button';
+import { Brain } from 'lucide-react';
 import React, { useState } from 'react'
 import { BtnBold, BtnBulletList, BtnItalic, BtnLink, BtnNumberedList, BtnRedo, BtnStrikeThrough, BtnUnderline, BtnUndo, Editor, EditorProvider, Separator, Toolbar } from 'react-simple-wysiwyg'
 
@@ -5,6 +7,11 @@ function RichTextEditor({onRichTextEditorChange}) {
     const [value, setValue] = useState();
     return (
         <div>
+            <div className='flex justify-between my-2'>
+                <label className='text-xs'>Summary</label>
+                <Button variant='outline'className='flex gap-2 border-primary text-primary'>
+                    <Brain className='h-4 w-4' /> Generate from AI</Button>
+            </div>
             <EditorProvider>
                 <Editor value={value} onChange={(e)=> {
                     setValue(e.target.value)
