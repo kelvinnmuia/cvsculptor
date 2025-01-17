1bb15e9379c3a165d7f678458cf7fa084b5e69d8
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-17 15:23:20 +0300

    changed the heading and sub heading for the education section

--- src/dashboard/cv/components/forms/Education.jsx
+++ src/dashboard/cv/components/forms/Education.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Education() {
   return (
-    <div>
-      
+    <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
+        <h2 className='font-bold text-lg'>Education</h2>
+        <p>Add Your educational details</p>
     </div>
   )
 }
