4f1c4d88cac79598db5fe91e7ceb76b1ac8c86a8
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-17 15:40:19 +0300

    added a field to capture university name

--- src/dashboard/cv/components/forms/Education.jsx
+++ src/dashboard/cv/components/forms/Education.jsx
@@ -16,6 +16,18 @@ function Education() {
     <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
         <h2 className='font-bold text-lg'>Education</h2>
         <p>Add Your educational details</p>
+        <div>
+            {educationalList.map((item,index)=>(
+                <div>
+                    <div>
+                        <div>
+                            <label>University Name</label>
+                            <Input name="universityName" onChange={()=>handleChange(e)}/>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
