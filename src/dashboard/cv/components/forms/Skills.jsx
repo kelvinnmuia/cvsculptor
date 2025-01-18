689154745f803281113f583935de509fa42b8f4a
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-18 22:14:51 +0300

    changed the content of the skills form heading and subheading

--- src/dashboard/cv/components/forms/Skills.jsx
+++ src/dashboard/cv/components/forms/Skills.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Skills() {
     return (
         <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
-            <h2 className='font-bold text-lg'>Professional Experience</h2>
-            <p>Add Your Previous Job(s) Experience</p>
+            <h2 className='font-bold text-lg'>Skills</h2>
+            <p>Add Your top skills</p>
         </div>
     )
 }
