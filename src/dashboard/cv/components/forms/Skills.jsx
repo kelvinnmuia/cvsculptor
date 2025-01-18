81f21f2d43a5e0de865ea13b0175a9b848b7fd37
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-18 22:13:00 +0300

    added the skills heading and subheading for the skills form

--- src/dashboard/cv/components/forms/Skills.jsx
+++ src/dashboard/cv/components/forms/Skills.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 function Skills() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
+            <h2 className='font-bold text-lg'>Professional Experience</h2>
+            <p>Add Your Previous Job(s) Experience</p>
+        </div>
+    )
 }
 
 export default Skills
