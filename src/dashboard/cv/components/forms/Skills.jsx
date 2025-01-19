08c345b0861cbbf78074ccff1203649ad4fa4038
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-19 14:19:52 +0300

    created the handle change function

--- src/dashboard/cv/components/forms/Skills.jsx
+++ src/dashboard/cv/components/forms/Skills.jsx
@@ -2,12 +2,15 @@ import { Input } from '@/components/ui/input'
 import React, { useState } from 'react'
 
 function Skills() {
-    const [skillsList,setSkillsList]=useState([
-        {
+    const [skillsList,setSkillsList]=useState([{
             name:'',
             rating:0
-        }
-    ])
+        }])
+
+    const handleChange=(index,name,value)=>{
+        
+    }
+
     return (
         <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
             <h2 className='font-bold text-lg'>Skills</h2>
