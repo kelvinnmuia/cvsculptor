8bf40fb1e37f1e5956651e83f577a8c724a87126
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-24 15:45:38 +0300

    added 10px margin above the download and share buttons

--- src/my-cv/[cvId]/view/index.jsx
+++ src/my-cv/[cvId]/view/index.jsx
@@ -10,7 +10,7 @@ function ViewCv() {
       <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
         <h2 className='text-center text-2xl font-medium'>Congrats! Your Ultimate AI generated Cv is ready</h2>
         <p className='text-center text-gray-400'>You can Now download and share your CV</p>
-        <div>
+        <div className='flex justify-between px-44 my-10'>
           <Button>Download</Button>
           <Button>Share</Button>
         </div>
