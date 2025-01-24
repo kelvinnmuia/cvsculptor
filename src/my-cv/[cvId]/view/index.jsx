d0282e88c3484c291a6389ca8a973fd7daf1ff63
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-24 15:41:04 +0300

    centered the p tag and changed its color to gray

--- src/my-cv/[cvId]/view/index.jsx
+++ src/my-cv/[cvId]/view/index.jsx
@@ -9,7 +9,7 @@ function ViewCv() {
 
       <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
         <h2 className='text-center text-2xl font-medium'>Congrats! Your Ultimate AI generated Cv is ready</h2>
-        <p>You can Now download and share your CV</p>
+        <p className='text-center text-gray-400'>You can Now download and share your CV</p>
         <div>
           <Button>Download</Button>
           <Button>Share</Button>
