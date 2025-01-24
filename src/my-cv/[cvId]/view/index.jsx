140a09b80d86d0a3771a712745bacbf70c3075e3
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-24 17:28:10 +0300

    added web share for the share button

--- src/my-cv/[cvId]/view/index.jsx
+++ src/my-cv/[cvId]/view/index.jsx
@@ -8,39 +8,51 @@ import GlobalApi from './../../../../service/GlobalApi'
 
 function ViewCv() {
 
-  const [cvInfo,setCvInfo]=useState();
-  const {cvId}=useParams();
-  
-  useEffect(()=>{
+  const [cvInfo, setCvInfo] = useState();
+  const { cvId } = useParams();
+
+  useEffect(() => {
     GetCvInfo();
   })
-  const GetCvInfo=()=>{{
-    GlobalApi.GetCvById(cvId).then(resp=>{
-      console.log(resp.data.data);
-      setCvInfo(resp.data.data);
-    })
-  }}
+  const GetCvInfo = () => {
+    {
+      GlobalApi.GetCvById(cvId).then(resp => {
+        console.log(resp.data.data);
+        setCvInfo(resp.data.data);
+      })
+    }
+  }
 
-  const HandleDownload=()=>{
+  const HandleDownload = () => {
     window.print();
   }
 
   return (
-    <CvInfoContext.Provider value={{cvInfo,setCvInfo}}>
+    <CvInfoContext.Provider value={{ cvInfo, setCvInfo }}>
       <div id="no-print">
-      <Header />
-      <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
-        <h2 className='text-center text-2xl font-medium'>Congrats! Your Ultimate AI generated Cv is ready</h2>
-        <p className='text-center text-gray-400'>You can Now download and share your CV</p>
-        <div className='flex justify-between px-44 my-10'>
-          <Button onClick={HandleDownload}>Download</Button>
-          <Button>Share</Button>
+        <Header />
+        <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
+          <h2 className='text-center text-2xl font-medium'>Congrats! Your Ultimate AI generated Cv is ready</h2>
+          <p className='text-center text-gray-400'>You can Now download and share your CV</p>
+          <div className='flex justify-between px-44 my-10'>
+            <Button onClick={HandleDownload}>Download</Button>
+
+            <RWebShare
+              data={{
+                text: "Hello Everyone!, This is my cv please check it out",
+                url: "https://on.natgeo.com/2zHaNup",
+                title: "Flamingos",
+              }}
+              onClick={() => console.log("shared successfully!")}
+            >
+              <Button>Share</Button>
+            </RWebShare>
+          </div>
         </div>
       </div>
+      <div id="print-area" className='my-10 mx-10 md:mx-20 lg:mx-36'>
+        <CvPreview />
       </div>
-      <div id="print-area">
-          <CvPreview />
-        </div>
     </CvInfoContext.Provider>
   )
 }
