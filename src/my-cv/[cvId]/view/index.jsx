efe15492d37c31aa5c811deef553346f942deb85
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-24 15:36:09 +0300

    added a p tag below the header

--- src/my-cv/[cvId]/view/index.jsx
+++ src/my-cv/[cvId]/view/index.jsx
@@ -9,6 +9,7 @@ function ViewCv() {
 
       <div className='my-10'>
         <h2 className='text-center text-2xl font-medium'>Congrats! Your Ultimate AI generated Cv is ready</h2>
+        <p>You can Now download and share your CV</p>
         <div>
           <Button>Download</Button>
           <Button>Share</Button>
