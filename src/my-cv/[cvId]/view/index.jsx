3acbaa1e11456cffce531fb0c247bfb382cdaa21
kelvinnmuia <kelvinnmuia@gmail.com>
2025-01-24 15:25:26 +0300

    added buttons to download and share cv on the view cv component

--- src/my-cv/[cvId]/view/index.jsx
+++ src/my-cv/[cvId]/view/index.jsx
@@ -1,10 +1,19 @@
 import Header from '@/components/custom/Header'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function ViewCv() {
   return (
     <div>
       <Header />
+
+      <div>
+        
+        <div>
+          <Button>Download</Button>
+          <Button>Share</Button>
+        </div>
+      </div>
     </div>
   )
 }
