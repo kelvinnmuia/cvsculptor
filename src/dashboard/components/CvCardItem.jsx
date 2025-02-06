f2423ba6f29e6df9ecfb60e521a5bcb7b87830f3
kelvinnmuia <kelvinnmuia@gmail.com>
2025-02-06 15:29:10 +0300

    changed added more padding to the dropdown menu

--- src/dashboard/components/CvCardItem.jsx
+++ src/dashboard/components/CvCardItem.jsx
@@ -22,31 +22,34 @@ import {
 
 function CvCardItem({ cv }) {
   return (
-    <Link to={'/dashboard/cv/' + cv.documentId + "/edit"}>
-      <div className='p-14 bg-gradient-to-b 
-      from-pink-100 via-grey-200 to-blue-200 
-      flex
-      items-center justify-center h-[280px] 
-      border-t-4 border-primary rounded-lg
-      hover:scale-105 transition-all hover:shadow-md shadow-primary'>
-        <img src="./cv.png" width={80} height={80} />
+    <div>
+      <Link to={'/dashboard/cv/' + cv.documentId + "/edit"}>
+        <div className='p-14 bg-gradient-to-b 
+        from-pink-100 via-grey-200 to-blue-200 
+        flex
+        items-center justify-center h-[280px] 
+        border-t-4 border-primary rounded-lg
+        hover:scale-105 transition-all hover:shadow-md shadow-primary'>
+          <img src="./cv.png" width={80} height={80} />
+        </div>
+      </Link>
+      <div>
+        <h2 className='text-center my-1'>{cv.title}</h2>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+          <MoreVertical className='h-4 w-4 cursor-pointer px-10' />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <h2 className='text-center my-1'>{cv.title}</h2>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-        <MoreVertical className='h-4 w-4 cursor-pointer px-10' />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-    </Link>
+    </div>
   )
 }
 
