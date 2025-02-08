02f23b4f24500b27b99e5321b269b9ddf81fd24d
kelvinnmuia <kelvinnmuia@gmail.com>
2025-02-08 23:15:48 +0300

    added comments and docs for the edit cv function

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
 const API_KEY=import.meta.env.VITE_STRAPI_API_KEY;
+/**
+ * Axios client instance configured with base URL and headers.
+ * 
+ * @constant {AxiosInstance} axiosClient - The Axios client instance.
+ * @property {string} baseURL - The base URL for the API.
+ * @property {Object} headers - The headers for the API requests.
+ * @property {string} headers.Content-Type - The content type of the request.
+ * @property {string} headers.Authorization - The authorization token for the API.
+ */
 const axiosClient=axios.create({
     baseURL:'http://localhost:1337/api/',
     headers:{
--- src/auth/sign-in/index.jsx
+++ src/auth/sign-in/index.jsx
@@ -1,6 +1,15 @@
 import { SignIn } from '@clerk/clerk-react'
 import React from 'react'
 
+/**
+ * SignInPage component renders a centered SignIn component.
+ *
+ * @component
+ * @example
+ * return (
+ *   <SignInPage />
+ * )
+ */
 function SignInPage() {
   return (
     <div className='flex justify-center my-20 items-center'>
